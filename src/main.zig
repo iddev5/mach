56d4326a6d4ac21e771c25705a13e420d1478c67
iddev5 <ayushbardhan5@gmail.com>
2022-06-22 23:56:41 +0530

    mach: Implement lower level file fetching API and base resource manager on it
    This has been designed with async model in mind. This API will help
    us to keep platform specific logic separate in resource manager and
    makes it easier to add new platforms. This low level API is not final.

--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,4 @@ pub usingnamespace @import("enums.zig");
 pub const Engine = @import("Engine.zig");
 pub const Timer = @import("Timer.zig");
 pub const ResourceManager = @import("resource/ResourceManager.zig");
+pub const Fetch = @import("resource/Fetch.zig");
---
+++ src/resource/Fetch.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const Fetch = @This();
+
+internal: GetFetchType(),
+
+pub fn init(allocator: std.mem.Allocator) !Fetch {
+    return Fetch{ .internal = try GetFetchType().init(allocator) };
+}
+
+pub fn deinit(fetch: *Fetch) void {
+    fetch.internal.deinit();
+}
+
+pub fn fetchFile(fetch: *Fetch, path: []const u8, ctx: *anyopaque, cb: fn (ctx: *anyopaque, mem: []const u8) void) !void {
+    try fetch.internal.fetchFile(path, ctx, cb);
+}
+
+fn GetFetchType() type {
+    if (builtin.cpu.arch != .wasm32) return @import("fetch_native.zig").PlatformFetch;
+}
+
+fn GetFileType() type {
+    return GetFetchType().PlatformFile;
+}
